08d7e3350dbe9cebffcaf7745cca41dc5ee96111
Ernesto Miralles <ernesto.miralles@mimacom.com>
2024-10-19 21:13:30 +0200

    chore: get rid of eslint flat config
    The ESLint VSCode extension does not yet work correctly with the new flat configuration introduced in recent versions of eslint.

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  plugins: ['react-hooks', 'react-refresh', 'prettier'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'prettier/prettier': 'warn',
+  },
+}
